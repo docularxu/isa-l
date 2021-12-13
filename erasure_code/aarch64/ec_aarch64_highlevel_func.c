7747b22961a28a0c24be26e01718c14d837f3820
Guodong Xu <guodong.xu@linaro.org>
2021-12-13 04:10:57 +0000

    erasure_code_perf: 5vect_dot_prod benchmarking
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- erasure_code/aarch64/ec_aarch64_highlevel_func.c
+++ erasure_code/aarch64/ec_aarch64_highlevel_func.c
@@ -166,18 +166,19 @@ void ec_encode_data_sve(int len, int k, int rows, unsigned char *g_tbls, unsigne
 		return;
 	}
 
-
+/*
 	while (rows >= 7) {
 		gf_7vect_dot_prod_sve(len, k, g_tbls, data, coding);
 		g_tbls += 7 * k * 32;
 		coding += 7;
 		rows -= 7;
 	}
-	while (rows >= 6) {
-		gf_6vect_dot_prod_sve(len, k, g_tbls, data, coding);
-		g_tbls += 6 * k * 32;
-		coding += 6;
-		rows -= 6;
+ */
+	while (rows >= 5) {
+		gf_5vect_dot_prod_sve(len, k, g_tbls, data, coding);
+		g_tbls += 5 * k * 32;
+		coding += 5;
+		rows -= 5;
 	}
 	while (rows >= 1) {
 		gf_vect_dot_prod_sve(len, k, g_tbls, data, *coding);
--- erasure_code/erasure_code_perf.c
+++ erasure_code/erasure_code_perf.c
@@ -107,10 +107,10 @@ int main(int argc, char *argv[])
 	struct perf start;
 
 	// Pick test parameters
-	m = 14;
+	m = 15;
 	k = 10;
-	nerrs = 4;
-	const u8 err_list[] = { 2, 4, 5, 7 };
+	nerrs = 5;
+	const u8 err_list[] = { 1, 2, 4, 5, 7};
 
 	printf("erasure_code_perf: %dx%d %d\n", m, TEST_LEN(m), nerrs);
 
